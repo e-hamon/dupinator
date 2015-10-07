8cef1a3dd4e455740b94e554f6b37a9a3eabc4f6
e-hamon <hamon.erwan@free.fr>
2015-10-07 21:22:28 +0200

    Create branch v2 for Andrew Shearer's version

--- dupinator.py
+++ dupinator.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 
+# Dupinator
+# Original script by Bill Bumgarner: see
+# http://www.pycs.net/bbum/2004/12/29/
+#
+# Updated by Andrew Shearer on 2004-12-31: see
+# http://www.shearersoftware.com/personal/weblog/2005/01/14/dupinator-ii
+
 import os
 import sys
 import stat
 import md5
 
 filesBySize = {}
+requireEqualNames = False
 
 def walker(arg, dirname, fnames):
     d = os.getcwd()
@@ -13,9 +21,9 @@ def walker(arg, dirname, fnames):
     try:
         fnames.remove('Thumbs')
     except ValueError:
-        pass
+        pass        
     for f in fnames:
-        if not os.path.isfile(f):
+        if not os.path.isfile(f) or os.path.islink(f) or f == '.DS_Store':
             continue
         size = os.stat(f)[stat.ST_SIZE]
         if size < 100:
@@ -32,44 +40,51 @@ for x in sys.argv[1:]:
     print 'Scanning directory "%s"....' % x
     os.path.walk(x, walker, filesBySize)    
 
+FIRST_SCAN_BYTES = 1024
 print 'Finding potential dupes...'
+dupes = [] # ashearer
 potentialDupes = []
 potentialCount = 0
-trueType = type(True)
 sizes = filesBySize.keys()
 sizes.sort()
 for k in sizes:
     inFiles = filesBySize[k]
-    outFiles = []
     hashes = {}
     if len(inFiles) is 1: continue
     print 'Testing %d files of size %d...' % (len(inFiles), k)
+    if requireEqualNames:
+        for fileName in inFiles:
+            hashes.setdefault(os.path.basename(fileName), []).append(fileName)
+        inFiles = []
+        for nameGroup in hashes.values():
+            if len(nameGroup) > 1:
+                inFiles.extend(nameGroup)
+        hashes = {}
     for fileName in inFiles:
-        if not os.path.isfile(fileName):
-            continue
+        #if not os.path.isfile(fileName):
+        #    continue
         aFile = file(fileName, 'r')
-        hasher = md5.new(aFile.read(1024))
+        hasher = md5.new(aFile.read(FIRST_SCAN_BYTES))
         hashValue = hasher.digest()
         if hashes.has_key(hashValue):
-            x = hashes[hashValue]
-            if type(x) is not trueType:
-                outFiles.append(hashes[hashValue])
-                hashes[hashValue] = True
-            outFiles.append(fileName)
+            hashes[hashValue].append(fileName)
         else:
-            hashes[hashValue] = fileName
+            hashes[hashValue] = [fileName]
         aFile.close()
-    if len(outFiles):
-        potentialDupes.append(outFiles)
-        potentialCount = potentialCount + len(outFiles)
+    outFileGroups = [fileGroup for fileGroup in hashes.values() if len(fileGroup) > 1] # ashearer
+    if k <= FIRST_SCAN_BYTES:   # we already scanned to whole file; put into definite dups list (ashearer)
+        dupes.extend(outFileGroups)
+    else:
+        potentialDupes.extend(outFileGroups)
+    potentialCount = potentialCount + len(outFileGroups)
 del filesBySize
 
 print 'Found %d sets of potential dupes...' % potentialCount
 print 'Scanning for real dupes...'
 
-dupes = []
+#dupes = [] ashearer
 for aSet in potentialDupes:
-    outFiles = []
+    #outFiles = []
     hashes = {}
     for fileName in aSet:
         print 'Scanning file "%s"...' % fileName
@@ -83,22 +98,21 @@ for aSet in potentialDupes:
         aFile.close()
         hashValue = hasher.digest()
         if hashes.has_key(hashValue):
-            if not len(outFiles):
-                outFiles.append(hashes[hashValue])
-            outFiles.append(fileName)
+            hashes[hashValue].append(fileName)  # ashearer
         else:
-            hashes[hashValue] = fileName
-    if len(outFiles):
-        dupes.append(outFiles)
+            hashes[hashValue] = [fileName] #ashearer
+    outFileGroups = [fileGroup for fileGroup in hashes.values() if len(fileGroup) > 1] # ashearer
+    dupes.extend(outFileGroups)
 
 i = 0
+bytesSaved = 0
 for d in dupes:
     print 'Original is %s' % d[0]
     for f in d[1:]:
         i = i + 1
-        print 'Deleting %s' % f
-        os.remove(f)
+        print 'Would have deleted %s' % f
+        bytesSaved += os.path.getsize(f)
+        #os.remove(f)
     print
-        
 
-        
+print "Would have saved %.1fK; %d file(s) duplicated." % (bytesSaved/1024.0,len(dupes))
