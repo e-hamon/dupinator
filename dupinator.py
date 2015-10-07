366e0eb4885bf2a0dbf507765a8c1b9d25f8c527
e-hamon <hamon.erwan@free.fr>
2015-10-07 23:47:02 +0200

    Version by Erwan Hamon - uses parser options

--- dupinator.py
+++ dupinator.py
@@ -1,104 +1,252 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Create a bash script to remove duplicate files
 
+Built from Bill Bumgarner's Dupinator recipe.
+http://code.activestate.com/recipes/362459/
+"""
 import os
-import sys
 import stat
-import md5
+import hashlib
+import filecmp
+from optparse import OptionParser
+import sys
 
-filesBySize = {}
+options = {}
+dirMove = "~/zzz"
 
-def walker(arg, dirname, fnames):
-    d = os.getcwd()
-    os.chdir(dirname)
-    try:
-        fnames.remove('Thumbs')
-    except ValueError:
-        pass
-    for f in fnames:
-        if not os.path.isfile(f):
-            continue
-        size = os.stat(f)[stat.ST_SIZE]
-        if size < 100:
-            continue
-        if filesBySize.has_key(size):
-            a = filesBySize[size]
-        else:
-            a = []
-            filesBySize[size] = a
-        a.append(os.path.join(dirname, f))
-    os.chdir(d)
+def scanDir(listDirs):
+    """
+    Scan all files in each directory from listDirs.
+    """
+    filesBySize = {}
 
-for x in sys.argv[1:]:
-    print 'Scanning directory "%s"....' % x
-    os.path.walk(x, walker, filesBySize)    
-
-print 'Finding potential dupes...'
-potentialDupes = []
-potentialCount = 0
-trueType = type(True)
-sizes = filesBySize.keys()
-sizes.sort()
-for k in sizes:
-    inFiles = filesBySize[k]
-    outFiles = []
-    hashes = {}
-    if len(inFiles) is 1: continue
-    print 'Testing %d files of size %d...' % (len(inFiles), k)
-    for fileName in inFiles:
-        if not os.path.isfile(fileName):
-            continue
-        aFile = file(fileName, 'r')
-        hasher = md5.new(aFile.read(1024))
-        hashValue = hasher.digest()
-        if hashes.has_key(hashValue):
-            x = hashes[hashValue]
-            if type(x) is not trueType:
-                outFiles.append(hashes[hashValue])
-                hashes[hashValue] = True
-            outFiles.append(fileName)
-        else:
-            hashes[hashValue] = fileName
-        aFile.close()
-    if len(outFiles):
-        potentialDupes.append(outFiles)
-        potentialCount = potentialCount + len(outFiles)
-del filesBySize
-
-print 'Found %d sets of potential dupes...' % potentialCount
-print 'Scanning for real dupes...'
-
-dupes = []
-for aSet in potentialDupes:
-    outFiles = []
-    hashes = {}
-    for fileName in aSet:
-        print 'Scanning file "%s"...' % fileName
-        aFile = file(fileName, 'r')
-        hasher = md5.new()
-        while True:
-            r = aFile.read(4096)
-            if not len(r):
-                break
-            hasher.update(r)
-        aFile.close()
-        hashValue = hasher.digest()
-        if hashes.has_key(hashValue):
-            if not len(outFiles):
-                outFiles.append(hashes[hashValue])
-            outFiles.append(fileName)
-        else:
-            hashes[hashValue] = fileName
-    if len(outFiles):
-        dupes.append(outFiles)
-
-i = 0
-for d in dupes:
-    print 'Original is %s' % d[0]
-    for f in d[1:]:
-        i = i + 1
-        print 'Deleting %s' % f
-        os.remove(f)
-    print
+    for dirname in listDirs:
+        print '# Scanning directory "%s"....' % dirname
+        #os.path.walk(x, walker, filesBySize)
         
+        for fname in os.listdir(dirname):
+            f = os.path.join(dirname, fname)
+            
+            if not os.path.isfile(f):
+                if options.debug: print "# " + f + " is not a file"
+                continue
+            
+            if os.path.islink(f):
+                if options.debug: print "# " + f + " is a symlink"
+                continue
+            
+            size = os.stat(f)[stat.ST_SIZE]
+            
+            if size == 0:
+                continue
+            
+            if filesBySize.has_key(size):
+                a = filesBySize[size]
+            else:
+                a = []
+                filesBySize[size] = a
+            a.append(f)
+    return filesBySize
 
+def scanDirs(listDirs):
+    """
+    Recursively scan all files in each directory from listDirs.
+    Used by the -R / --recursive parser option
+    """
+    filesBySize = {}
+    
+#    if options.modeKeep1:
+#        print "# Keep files in %s." % listDirs[0]
+    
+    for x in listDirs:
+        print '# Scanning directory "%s"....' % x
         
+        for dirname, dummyDirs, fnames in os.walk(x):
+            for fname in fnames:
+                f = os.path.join(dirname, fname)
+                
+                if not os.path.isfile(f):
+                    if options.debug: print "# " + f + " is not a file"
+                    continue
+                
+                if os.path.islink(f):
+                    if options.debug: print "# " + f + " is a symlink"
+                    continue
+                
+                size = os.stat(f)[stat.ST_SIZE]
+                
+                if size == 0:
+                    continue
+                
+                if filesBySize.has_key(size):
+                    a = filesBySize[size]
+                else:
+                    a = []
+                    filesBySize[size] = a
+                a.append(f)
+    return filesBySize
+
+def findDuplicates(filesBySize):
+    print '# Finding potential dupes...'
+    
+    dupes = []
+    total = len(filesBySize)
+    
+    while len(filesBySize):
+        (k, inFiles) = filesBySize.popitem()
+        #outFiles = []
+        hashes = {}
+        if len(inFiles) is 1: continue
+        inFiles.sort()
+        prevFile = ""
+        if options.debug:
+            print '# %d / %d Testing %d files of size %d.' % (len(filesBySize), total, len(inFiles), k)
+        elif options.verbose:
+            sys.stderr.write('# %d / %d Testing %d files of size %d.\n' % (len(filesBySize), total, len(inFiles), k))
+            
+        for fileName in inFiles:
+            if not os.path.isfile(fileName):
+                continue
+            if (fileName == prevFile):
+                if options.debug:
+                    print "Duplicate " + fileName
+                continue
+            prevFile = fileName
+            try:
+                aFile = file(fileName, 'r')
+            except:
+                print "# Cannot open file %s !!!" % (fileName)
+                continue
+            #hasher = md5.new(aFile.read(1024))
+            #hashValue = hasher.digest()
+            hashValue = hashlib.md5(aFile.read(1024)).digest() # 4096 ?
+            aFile.close()
+            if hashes.has_key(hashValue):
+                if options.debug:
+                    print "  # hash found"
+                x = hashes[hashValue]
+                isDup = False
+                for dbl in x:
+                    if options.debug:
+                        print "    # filecmp"
+                    if filecmp.cmp(fileName, dbl[0], False):
+                        dbl.append(fileName)
+                        isDup = True
+                        if options.debug:
+                            print "      # dupes %s\n              %s" % (dbl[0], fileName)
+                        break
+                    else:
+                        if options.debug:
+                            print "      # difs %s\n             %s" % (dbl[0], fileName)
+                       
+                if not isDup:
+                    x.append([fileName])
+            else:
+                if options.debug:
+                    print "  # new hash"
+                hashes[hashValue] = [[fileName]]
+        
+        while len(hashes):
+            (dummy, listF) = hashes.popitem()
+            while len(listF):
+                listF2 = listF.pop()
+                if len(listF2) > 1:
+                    listF2.sort()
+                    #listF2.reverse()
+                    dupes.append(listF2)
+    return dupes
+
+def outputScript(dupes, listDirs):
+    dupes.sort()
+    dupes.reverse()
+    
+    #for d in dupes:
+    while dupes:
+        d = dupes.pop() # Unstack dupes until it's empty
+        found = False
+        if options.modeKeep1:
+            for f in d[:]:      # Loop on a copy of d to be able to modify it
+                                # d is the duplicates' list.
+                if f.rfind(listDirs[0]) == 0:
+                    found = True
+                    print '#     "%s"' % f
+                    d.remove(f)
+    
+        if options.modeKmail and not found:
+            for f in d[:]:      # Loop on a copy of d to be able to modify it
+                                # d is the duplicates' list.
+                if f[-5:] == ":2,RS":   # Keep answered emails in priority
+                    found = True
+                    print '#     "%s"' % f
+                    d.remove(f)
+                    break
+        
+        if options.modeKmail and not found:
+            for f in d[:]:      # Loop on a copy of d to be able to modify it
+                                # d is the duplicates' list.
+                if f[-4:] == ":2,S":   # Keep read emails in 2nd priority
+                    found = True
+                    print '#     "%s"' % f
+                    d.remove(f)
+                    break
+        
+        if not found:
+            f = d.pop(0)        # Save the first one if none else
+            print '#     "%s"' % f
+            
+        for f in d:
+            if options.modeMove:
+                print 'mv "%s" %s' % (f, os.path.join(dirMove, f))
+                if not os.path.exists(os.path.dirname(os.path.join(dirMove, f))):
+                    os.makedirs(os.path.dirname(os.path.join(dirMove, f)))
+            else:
+                print 'rm -fv "%s"' % f
+        #        os.remove(f)
+        print
+
+
+def main():
+    global options
+
+    parser = OptionParser()
+    parser.set_usage("usage: %prog [options] [dir]...")
+    parser.add_option("-1", "--keep1",  
+                    action="store_true", dest="modeKeep1", default=False,
+                    help="Keep files in first target directory")
+    parser.add_option("-m", "--move",
+                    action="store_true", dest="modeMove", default=False,
+                    help="Move duplicate files to ~/zzz instead of deleting them")
+    parser.add_option("-k", "--kmail",
+                    action="store_true", dest="modeKmail", default=False,
+                    help="In kmail maildirs, delete unread mails first")
+    parser.add_option("-R", "--recursive",
+                    action="store_true", dest="recursive", default=False,
+                    help="Visit subdirectories recursively.")
+    parser.add_option("-d", "--debug",
+                    action="store_true", dest="debug", default=False,
+                    help="Verbose output for debugging")
+    parser.add_option("-v", "--verbose",
+                    action="store_true", dest="verbose", default=False,
+                    help="Verbose output but less than debug")
+                    
+    (options, listDirs) = parser.parse_args()
+    
+    if not len(listDirs):
+        listDirs.append(".")    # Default to current directory.
+    
+    print '#!/bin/bash'
+    
+    if options.modeKeep1:
+        print "# Keep files in %s." % listDirs[0]
+
+    if options.recursive:
+        filesBySize = scanDirs(listDirs)
+    else:
+        filesBySize = scanDir(listDirs)
+    
+    dupes = findDuplicates(filesBySize)
+    outputScript(dupes, listDirs)
+
+if __name__=='__main__': main()
